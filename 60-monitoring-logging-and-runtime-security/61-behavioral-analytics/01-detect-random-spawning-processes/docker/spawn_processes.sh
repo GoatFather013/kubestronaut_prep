4c9ec1b3df594f6a3bc46b7466b931f93f64d626
GoatFather013 <flsameulenberg@hotmail.com>
2024-09-05 08:21:10 +0200

    61-behavioral-analytics

--- 60-monitoring-logging-and-runtime-security/61-behavioral-analytics/01-detect-random-spawning-processes/docker/spawn_processes.sh
+++ 60-monitoring-logging-and-runtime-security/61-behavioral-analytics/01-detect-random-spawning-processes/docker/spawn_processes.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+LOG_FILE="/var/log/spawn_processes.log"
+
 while true; do
   # Generate a random sleep interval between 1 and 10 seconds
   sleep_interval=$((RANDOM % 10 + 1))
   sleep $sleep_interval
 
   # Spawn a new process (e.g., a simple sleep command)
-  commands=("curl" "ls -l" "nc -zv 8.8.8.8 53" "wget" "nmap")
+  commands=(
+    "nc -l -p 1234 || true"  # Listening on a network port
+    "sh -c 'echo Hello' || true"  # Shell spawning from a non-shell parent
+    "ls / || true"  # Listing root directory
+    "cat /proc/cpuinfo || true"  # Reading CPU info
+    "chmod 777 /tmp || true"  # Modifying permissions of a temporary directory
+  )
   random_command=${commands[$RANDOM % ${#commands[@]}]}
-  eval "$random_command" &
-  echo "Spawned a new process with PID $!"
+  eval "$random_command" >> "$LOG_FILE" 2>&1 &
+  echo "Spawned a new process with PID $!" >> "$LOG_FILE" 2>&1
 done
