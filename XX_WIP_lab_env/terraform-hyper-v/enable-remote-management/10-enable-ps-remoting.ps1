25e2b4153471b82ff321c1fec85a8328e19de1e9
GoatFather013 <flsameulenberg@hotmail.com>
2024-09-20 15:39:49 +0200

    weekend push

---
+++ XX_WIP_lab_env/terraform-hyper-v/enable-remote-management/10-enable-ps-remoting.ps1
@@ -0,0 +1,6 @@
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+
+Set-WSManInstance WinRM/Config/WinRS -ValueSet @{MaxMemoryPerShellMB = 1024}
+Set-WSManInstance WinRM/Config -ValueSet @{MaxTimeoutms=1800000} # Profile needs to be private or domain
+Set-WSManInstance WinRM/Config/Client -ValueSet @{TrustedHosts="*"}
+Set-WSManInstance WinRM/Config/Service/Auth -ValueSet @{Negotiate = $true}
